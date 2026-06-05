cef34c22ddc086cce8108fc55cc1319cbbcd475e
Goesg <diegogoessg@gmail.com>
2026-06-05 20:05:02 -0300

    chore: corrigir compilar.bat para detectar Visual Studio 18 e versões anteriores
    Atualiza o loop de busca do MSBuild para verificar as versões 18, 2022
    e 2019 do Visual Studio, resolvendo falha de detecção em instalações
    que não seguem o padrão de nomenclatura esperado.

--- compilar.bat
+++ compilar.bat
@@ -6,16 +6,18 @@ set EXECUTAVEL=%~dp0build\x64\Debug\Demo3D.exe
 
 echo === Compilando Demo3D (Debug x64) ===
 
-REM Tentar localizar o MSBuild do Visual Studio 2022
+REM Procurar MSBuild em todas as versões e edições do Visual Studio
 set MSBUILD=
-for %%e in (Community Professional Enterprise) do (
-    if exist "C:\Program Files\Microsoft Visual Studio\2022\%%e\MSBuild\Current\Bin\MSBuild.exe" (
-        set "MSBUILD=C:\Program Files\Microsoft Visual Studio\2022\%%e\MSBuild\Current\Bin\MSBuild.exe"
-        goto :encontrado
+for %%v in (18 2022 2019 17) do (
+    for %%e in (Community Professional Enterprise) do (
+        if exist "C:\Program Files\Microsoft Visual Studio\%%v\%%e\MSBuild\Current\Bin\MSBuild.exe" (
+            set "MSBUILD=C:\Program Files\Microsoft Visual Studio\%%v\%%e\MSBuild\Current\Bin\MSBuild.exe"
+            goto :encontrado
+        )
     )
 )
 
-echo ERRO: MSBuild nao encontrado. Instale o Visual Studio 2022.
+echo ERRO: MSBuild nao encontrado. Abra este .bat pelo Developer PowerShell do Visual Studio.
 pause
 exit /b 1
 
